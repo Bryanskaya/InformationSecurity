7fd6d7795efdbbc3cdf4388763bcd5935e6d3d6b
Bryanskaya <bryanskayakatya@yandex.ru>
2021-09-12 12:33:14 +0300

    ADDED: lab 1

---
+++ lab01/installer/Installer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+
+namespace lab01
+{
+    public class Installer
+    {
+        static string getId()
+        {
+            string query = "SELECT SerialNumber FROM Win32_BIOS";
+            string res = "";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+            foreach (ManagementObject info in searcher.Get())
+            {
+                res = info["SerialNumber"].ToString();
+            }
+
+            return res;
+        }
+
+        static string getHash(string serialNum)
+        {
+            UnicodeEncoding ue = new UnicodeEncoding();
+            SHA256 shHash = SHA256.Create();
+
+            byte[] hashValue = shHash.ComputeHash(ue.GetBytes(serialNum));
+
+            return Convert.ToBase64String(hashValue);
+        }
+
+        static string getKey()
+        {
+            return getHash(getId());
+        }
+
+        public static bool cmpKeys(string key)
+        {
+            string data = getKey();
+            return key.Equals(data);
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\key2.txt";
+            byte[] keyBytes = Encoding.Default.GetBytes(getKey());
+
+            FileStream fstream = new FileStream(path, FileMode.OpenOrCreate);
+
+            fstream.Write(keyBytes, 0, keyBytes.Length);
+            fstream.Close();
+        }
+    }
+}
---
+++ lab01/program/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (testAuthen())
+                Console.WriteLine("Hello, {0}! Authentication was successful.", Environment.UserName);
+            else
+                Console.WriteLine("ERROR: action is forbidden!");
+
+            Console.ReadKey();
+        }
+
+        static bool testAuthen()
+        {
+            string path = @"C:\msys64\home\bryan\InformationSecurity\lab01\key.txt";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Please, run the installer first!");
+                return false;
+            }
+
+            string key = File.ReadAllText(path);
+
+            if (Installer.cmpKeys(key))
+                return true;
+
+            return false;
+        }
+    }
+}
