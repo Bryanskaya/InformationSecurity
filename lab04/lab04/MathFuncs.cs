a1fa0c76035403a0162b53ce2066075579fedad8
Bryanskaya <bryanskayakatya@yandex.ru>
2021-10-24 00:13:26 +0300

    STARTED: lab 4

---
+++ lab04/lab04/MathFuncs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    class MathFuncs
+    {
+        public static bool IsSimple(int num)
+        {
+            double temp = Math.Sqrt(num);
+
+            for (int i = 2; i < temp; i++)
+                if (num % i == 0)
+                    return false;
+
+            return true;                
+        }
+
+        public static int fi(int p, int q)
+        {
+            return (p - 1) * (q - 1);
+        }
+
+        public static int GCD(int a, int b)
+        {
+            int tempA;
+
+            while (b != 0)
+            {
+                tempA = b;
+                b = a - b;
+                a = tempA;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ lab04/lab04/RSA.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    class RSA
+    {
+        private int _startKey = 1000;
+        private int _endKey = 9999;
+        private int privateKey;
+        public int publicKey;
+        
+        static Random rnd = new Random();
+
+
+        public RSA()
+        {
+            int left = 1;
+
+            int p = GetSimpleKey(_startKey, _endKey);
+            int q = GetSimpleKey(_startKey, _endKey);
+            int n = p * q;
+
+            publicKey = GetPublicKey(left, MathFuncs.fi(p, q));
+            privateKey = GetPrivateKey();
+        }
+
+        static int GetSimpleKey(int start, int end)
+        {
+            int num;
+
+            do
+            {
+                num = rnd.Next(start, end);
+            } while (!MathFuncs.IsSimple(num));
+
+            return num;
+        }
+
+        static int GetPublicKey(int fi, int left = 1)
+        {
+            int e, res;
+
+            do
+            {
+                e = rnd.Next(left + 1, fi);
+                res = MathFuncs.GCD(e, fi);
+            } while (res != 1);
+
+            return e;            
+        }
+
+        static int GetPrivateKey()
+        {
+            int res;
+
+            return res;
+        }
+    }
+}
