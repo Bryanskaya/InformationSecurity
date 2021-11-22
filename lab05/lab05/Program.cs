79776067182f896a2cfe4021a52dbab27f92b2c3
Bryanskaya <bryanskayakatya@yandex.ru>
2021-11-22 20:59:30 +0300

    ADDED: lab 5

---
+++ lab05/lab05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileNameScr = @"tests/text.txt";                //@"test_video.gif"; @"img.jpg"; @"text.txt";
+            string fileNameCipher = @"tests/ciphered_version.txt";
+            string fileNameResult = @"tests/uncipher_version.txt";
+
+            Cipher cipher = new Cipher();
+
+            cipher.Encrypt(fileNameScr, fileNameCipher);
+            cipher.Decrypt(fileNameCipher, fileNameResult);
+
+            Console.WriteLine("Press any button");
+            Console.ReadKey();
+        }
+    }
+}
