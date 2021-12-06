3ff93d78c8aec9d71a0f6ddd7cab73f56fdc6d6a
Bryanskaya <bryanskayakatya@yandex.ru>
2021-12-07 01:13:16 +0300

    ADDED: lab 5

--- lab05/lab05/Program.cs
+++ lab05/lab05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RW;
 
 namespace lab05
 {
@@ -10,14 +11,20 @@ namespace lab05
     {
         static void Main(string[] args)
         {
+            string fileEncrypt = @"encryptKey.txt";
+            string fileDecrypt = @"decryptKey.txt";
+
             string fileNameScr = @"tests/text.txt";                //@"test_video.gif"; @"img.jpg"; @"text.txt";
-            string fileNameCipher = @"tests/ciphered_version.txt";
-            string fileNameResult = @"tests/uncipher_version.txt";
+            string fileNameSigned = @"tests/signed_version.txt";
 
-            Cipher cipher = new Cipher();
+            Signature sign = new Signature(fileEncrypt, fileDecrypt);
+            sign.Sign(fileNameScr, fileNameSigned);
 
-            cipher.Encrypt(fileNameScr, fileNameCipher);
-            cipher.Decrypt(fileNameCipher, fileNameResult);
+            byte s = 69; /* E */
+            //Writer.addSign(fileNameScr, s);
+
+            Signature check = new Signature(fileDecrypt);
+            check.CheckData(fileNameSigned, fileNameScr);
 
             Console.WriteLine("Press any button");
             Console.ReadKey();
